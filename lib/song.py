3f5af52ae60739477ff006a6926c751dd55e0b44
Kervin Reginald Philippe Robergeau <robergeaukervin@gmail.com>
2025-09-21 23:02:11 -0400

    lab done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,52 @@
 class Song:
-    pass
+    # Attributs de classe
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Appeler les méthodes de classe pour mettre à jour les attributs
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+    
+    @classmethod
+    def add_song_to_count(cls):
+        """Incrémente le compteur de chansons"""
+        cls.count += 1
+    
+    @classmethod
+    def add_to_genres(cls, genre):
+        """Ajoute un genre à la liste des genres (sans doublons)"""
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        """Ajoute un artiste à la liste des artistes (sans doublons)"""
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+    
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        """Met à jour le compteur de chansons par genre"""
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+    
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        """Met à jour le compteur de chansons par artiste"""
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
